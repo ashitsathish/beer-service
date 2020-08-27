a94f0e106e8ee4b9a2c0f796ce6901b85cc2e28b
ashitsathish <ashitsathish8118@gmail.com>
2020-08-27 15:01:44 -0400

    added bean validation to controllers and exception handler code.

---
+++ src/main/java/com/ashitsathish/beerservice/web/controller/MVCExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ashitsathish.beerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ashitsathish on Aug,2020
+ */
+@ControllerAdvice
+public class MVCExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validateErrorHandler(ConstraintViolationException e){
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+        e.getConstraintViolations().forEach(error->{
+            errors.add(error.toString());
+        });
+
+        return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
+    }
+}
