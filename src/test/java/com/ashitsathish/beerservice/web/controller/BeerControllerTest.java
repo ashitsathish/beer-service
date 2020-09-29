0dcdc0ef0fc8d73607626636474f92e3a7376828
ashitsathish <ashitsathish8118@gmail.com>
2020-09-28 20:03:27 -0400

    Fixed Broken Tests #3

--- src/test/java/com/ashitsathish/beerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/ashitsathish/beerservice/web/controller/BeerControllerTest.java
@@ -2,6 +2,7 @@ package com.ashitsathish.beerservice.web.controller;
 
 
 import com.ashitsathish.beerservice.web.model.BeerDto;
+import com.ashitsathish.beerservice.web.model.BeerStyleEnum;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -31,7 +33,7 @@ class BeerControllerTest {
     }
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getBeerObject();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
         mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -40,12 +42,17 @@ class BeerControllerTest {
     }
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getBeerObject();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+
+    private BeerDto getBeerObject(){
+       return BeerDto.builder().beerName("Model").beerStyle(BeerStyleEnum.ALE).price(new BigDecimal(12.121)).upc(1231L).build();
     }
 }
